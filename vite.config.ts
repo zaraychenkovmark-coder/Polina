4670ea07481a2361ad187b9b50199be89a81ae99
samultra <samultra7@gmail.com>
2025-11-19 21:37:43 +0300

    fix: set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/Polina/",
   server: {
     host: "::",
     port: 8080,
